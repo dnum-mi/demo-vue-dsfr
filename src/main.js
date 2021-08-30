997feecc57027bf27f2be1bcff61514b0e63fe05
Stanislas Ormières <stan@stormier.ninja>
2021-08-30 17:00:36 +0200

    refactor: :recycle: Reorganise les imports
    - CSS d'abord : le CSS venant de node_modules d'abord
    - puis les modules venant de node_modules
    - puis les modules de l'app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router/index.js'
-import VueDsfr from '@laruiss/vue-dsfr'
-
 import '@laruiss/vue-dsfr/dist/vue-dsfr-fonts.css'
 import '@laruiss/vue-dsfr/dist/vue-dsfr.css'
+
+import { createApp } from 'vue'
+import VueDsfr from '@laruiss/vue-dsfr'
+
+import App from './App.vue'
+import router from './router/index.js'
 import './icons.js'
 
 createApp(App)
